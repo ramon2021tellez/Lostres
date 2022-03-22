6d96f0feff08e56c7e3a2a5a579a8956ef2445fc
ramon2021tellez <miranda.tellez@gmail.com>
2022-03-22 20:47:33 -0300

    four commit

---
+++ juego.rb
@@ -0,0 +1,35 @@
+#0 es piedra 
+#1 es papel
+#2 es tijera
+#ingresa una opcion ARGV entre piedra papel o tijera
+
+pc = rand(0..2)
+usuario = ARGV[0].downcase
+if usuario== "piedra"
+if pc == 0
+    puts "comptador juega piedra"
+    puts "empataste"
+elseif
+   pc == 1
+   puts "computador juega papel"
+   puts "perdists"
+else
+    pc== 2
+    puts "computador juega tijera"
+    puts "ganaste"
+end
+if usuario== "papel"
+if pc == 0
+    puts "comptador juega piedra"
+    puts "ganaste"
+elseif
+   pc == 1
+   puts "computador juega papel"
+   puts "empataste"
+else
+    pc== 2
+    puts "computador juega tijera"
+    puts "perdiste"
+end
+puts "argumento invalido"
+end
