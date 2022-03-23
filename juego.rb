883829fc18aae91909e0d6bfe3462bfd16638a3f
ramon2021tellez <miranda.tellez@gmail.com>
2022-03-22 21:05:48 -0300

    new commit

--- juego.rb
+++ juego.rb
@@ -9,8 +9,7 @@ if usuario == "piedra"
 if pc == 0
     puts "comptador juega piedra"
     puts "empataste"
-elseif
-   pc == 1
+elseif pc == 1
    puts "computador juega papel"
    puts "perdists"
 else
@@ -22,12 +21,10 @@ if usuario == "papel"
 if pc == 0
     puts "comptador juega piedra"
     puts "ganaste"
-elseif
-   pc == 1
+elseif pc == 1
    puts "computador juega papel"
    puts "empataste"
-elseif
-    pc == 2
+elseif pc == 2
     puts "computador juega tijera"
     puts "perdiste"
 end
