e664eee88f19d3df0c4612769f63678a98a5fe13
ramon2021tellez <miranda.tellez@gmail.com>
2022-03-22 22:24:07 -0300

    first commit

--- mayor4.rb
+++ mayor4.rb
@@ -1,17 +1,21 @@
 num1 = ARGV[0].to_i
 num2 = ARGV[1].to_i
 num3 = ARGV[2].to_i
-
+num4 = ARGV[3].to_i
 #if num1 > num2
-    puts num1
+#    puts num1
 #elsif num2 > num3
 #    puts num2
     
 #else
 #    puts num3
 #end
+puts num4.class
 
-mayor = num1
+if !num4.nill?
+    num4.to_i
+    mayor = num4 if mayor < num4
+end
 
 mayor = num2 if mayor < num2
 
