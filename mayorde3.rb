d0ca376b34b16e6f7246a67a68e172ac5666ffd5
ramon2021tellez <miranda.tellez@gmail.com>
2022-03-22 21:31:45 -0300

    new commit

---
+++ mayorde3.rb
@@ -0,0 +1,13 @@
+num1 = ARGV[0]
+num2 = ARGV[1]
+num3 = ARGV[2]
+
+if num1 > num2
+    puts "numero uno es mayor"
+elsif num1> num3
+    puts "numero uno es mayor"
+    
+else
+    puts "numero tres es mayor"
+end
+    puts "numero dos es mayor"
